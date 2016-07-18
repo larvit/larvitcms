cb6172de5ec6a1a5dd2f41f23f32a35fdc30653c
lilleman <lilleman@larvit.se>
2016-07-18 14:14:07 +0200

    Added default cms controller for public page

---
+++ controllers/cms.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const lfs = require('larvitfs'),
+      cms = require('larvitcms'),
+      log = require('winston');
+
+function call404(req, res, cb) {
+	res.templateName = '404';
+
+	if (lfs.getPathSync('controllers/404.js')) {
+		require(lfs.getPathSync('controllers/404.js')).run(req, res, cb);
+	} else {
+		const err = new Error('larvitcms: controllers/cms.js - call404() - 404 controller not found');
+		res.statusCode = 500;
+		data = {};
+		cb(err, req, res, {});
+	}
+}
+
+exports.run = function(req, res, cb) {
+	const data = {'global': res.globalData};
+
+	if (req.lang === undefined) {
+		if (res.langs !== undefined && res.langs[0] !== undefined) {
+			req.lang = res.langs[0];
+		} else {
+			req.lang = 'en';
+		}
+	}
+
+	if ( ! data.global.title) {
+		data.global.title = '';
+	}
+
+	if (req.urlParsed.path.substring(req.urlParsed.path.length - 5) === '.json') {
+		req.urlParsed.path = req.urlParsed.path.substring(0, req.urlParsed.path.length - 5);
+	}
+
+	cms.getPages({'slugs': req.urlParsed.path}, function(err, pages) {
+		if (err) {
+			cb(err, req, res, data);
+			return;
+		}
+
+		if (pages.length === 0) {
+			log.verbose('larvitcms: controllers/cms.js - CMS controller called, but no page found for slug: "' + req.urlParsed.path + '"');
+			call404(req, res, cb);
+
+			return;
+		}
+
+		// Take the first page that is found... if there are several, well, tough luck :D
+		if (pages[0] !== undefined && pages[0].langs !== undefined && pages[0].langs[req.lang] !== undefined) {
+			data.cmsData       = pages[0].langs[req.lang];
+			data.global.title += ' | ' + data.cmsData.htmlTitle;
+			data.global.slugs = {};
+			for (const key in pages[0].langs) {
+				data.global.slugs[key] = pages[0].langs[key].slug;
+			}
+		} else {
+			log.verbose('larvitcms: controllers/cms.js - CMS controller called, but no content found for slug: "' + req.urlParsed.path + '" and lang: "' + req.lang + '"');
+			call404(req, res, cb);
+			return;
+		}
+
+		cb(err, req, res, data);
+	});
+};
