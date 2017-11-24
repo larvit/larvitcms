2d37b9785b05284a8f0f05476407f2f472d98ec8
Gagge <tobias@nublar.se>
2017-11-24 12:08:00 +0100

    Removed previous fix that made slugs case sensative

--- dbmigration/7.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `cms_snippets`
-CHANGE `slug` `slug` varchar(100) COLLATE 'utf8mb4_bin' NOT NULL FIRST,
-CHANGE `lang` `lang` char(2) COLLATE 'utf8mb4_bin' NOT NULL DEFAULT 'en' AFTER `slug`;
